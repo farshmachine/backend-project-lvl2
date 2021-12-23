da40f7df6dee90002a4a7216db01a220f00beec6
farshmachine <m4ghty@yandex.ru>
2021-12-23 11:15:25 -0800

    added format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,11 @@
 import { Command } from 'commander/esm.mjs';
 const program = new Command();
 
-program.version('0.0.1');
+program
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format [type]', 'output format')
 
 program.parse();
-
