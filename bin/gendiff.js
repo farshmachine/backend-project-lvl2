e38e8698069f695ed3cfc2eae9d41667f0ac562c
mmakarevich <m4ghty@yandex.ru>
2021-12-30 12:05:30 +0300

    added eslint config

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander/esm.mjs';
+import { Command } from 'commander/esm';
 import {
-    getFilesData,
-    mergeObjectValues,
-    prepareOutput
-} from '../src/comparators.js';
+  getFilesData,
+  mergeObjectValues,
+  prepareOutput,
+} from '../src/comparators';
 
 const program = new Command();
 
 program
-    .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    .option('-f, --format [type]', 'output format')
-    .action((filepath1, filepath2) => {
-        const data = getFilesData(filepath1, filepath2);
-        const diff = mergeObjectValues(data);
-        const output = prepareOutput(diff);
-        console.log(output);
-      });
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    const data = getFilesData(filepath1, filepath2);
+    const diff = mergeObjectValues(data);
+    const output = prepareOutput(diff);
+    console.log(output);
+  });
 
 program.parse();
