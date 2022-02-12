fbf745cb85e06fc970a967b2808fa09da09c5066
farshmachine <m4ghty@yandex.ru>
2022-02-12 21:05:13 +0300

    added format option default value

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     const diff = getDiff(filepath1, filepath2);
     console.log(diff);
