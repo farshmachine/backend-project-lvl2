8ebe48a2fe5a8a601cfdbad7e4668e02171ee641
farshmachine <m4ghty@yandex.ru>
2022-02-18 11:19:05 +0300

    added comparator function tests

--- __tests__/comparator.test.js
+++ __tests__/comparator.test.js
@@ -1,19 +1,75 @@
 #!/usr/bin/env node
 
-const jsonDiff = require('../src/comparators');
+import getObjectsDiff from '../src/comparator';
+import { expect } from '@jest/globals';
 
-describe('json flat comparator', () => {
-  it('test', () => {
-    const file1Path = './__fixtures__/file1.json';
-    const file2Path = './__fixtures__/file2.json';
+describe('comparator', () => {
+  it('inner key type should be modified', () => {
+    const obj1 = {
+      key: {
+        innerKey: 'foo',
+      },
+    };
 
-    expect(jsonDiff(file1Path, file2Path)).toEqual(`{
-      - follow: false
-        host: hexlet.io
-      - proxy: 123.234.53.22
-      - timeout: 50
-      + timeout: 20
-      + verbose: true
-    }`);
+    const obj2 = {
+      key: {
+        innerKey: {
+          foo: '42',
+        },
+      },
+    };
+
+    const [diff] = getObjectsDiff(obj1, obj2);
+    const [innerKeyDiff] = diff.children;
+    expect(innerKeyDiff.type).toBe('modified');
+  });
+
+  it('inner key type should be equal', () => {
+    const obj1 = {
+      key: {
+        innerKey: 'foo',
+      },
+    };
+
+    const obj2 = {
+      key: {
+        innerKey: 'foo',
+      },
+    };
+
+    const [diff] = getObjectsDiff(obj1, obj2);
+    const [innerKeyDiff] = diff.children;
+    expect(innerKeyDiff.type).toBe('equal');
+  });
+
+  it('inner key type should be deleted', () => {
+    const obj1 = {
+      key: {
+        innerKey: 'foo',
+      },
+    };
+
+    const obj2 = {
+      key: {},
+    };
+
+    const [diff] = getObjectsDiff(obj1, obj2);
+    const [innerKeyDiff] = diff.children;
+    expect(innerKeyDiff.type).toBe('deleted');
+  });
+  it('inner key type should be added', () => {
+    const obj1 = {
+      key: {},
+    };
+
+    const obj2 = {
+      key: {
+        innerKey: 'foo',
+      },
+    };
+
+    const [diff] = getObjectsDiff(obj1, obj2);
+    const [innerKeyDiff] = diff.children;
+    expect(innerKeyDiff.type).toBe('added');
   });
 });
