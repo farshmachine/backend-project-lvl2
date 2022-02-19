b19331c5842983de0afdcd0e2b253e10c63644fc
farshmachine <m4ghty@yandex.ru>
2022-02-19 23:23:48 +0300

    added comparator tests for nested objects

--- __tests__/comparator.test.js
+++ __tests__/comparator.test.js
@@ -1,51 +1,51 @@
 #!/usr/bin/env node
 
-import getObjectsDiff from '../src/comparator';
-import { expect } from '@jest/globals';
+import { expect } from "@jest/globals";
+import getObjectsDiff from "../src/comparator";
 
-describe('comparator', () => {
-  it('inner key type should be modified', () => {
+describe("comparator", () => {
+  it("inner key type should be modified", () => {
     const obj1 = {
       key: {
-        innerKey: 'foo',
+        innerKey: "foo",
       },
     };
 
     const obj2 = {
       key: {
         innerKey: {
-          foo: '42',
+          foo: "42",
         },
       },
     };
 
     const [diff] = getObjectsDiff(obj1, obj2);
     const [innerKeyDiff] = diff.children;
-    expect(innerKeyDiff.type).toBe('modified');
+    expect(innerKeyDiff.type).toBe("modified");
   });
 
-  it('inner key type should be equal', () => {
+  it("inner key type should be equal", () => {
     const obj1 = {
       key: {
-        innerKey: 'foo',
+        innerKey: "foo",
       },
     };
 
     const obj2 = {
       key: {
-        innerKey: 'foo',
+        innerKey: "foo",
       },
     };
 
     const [diff] = getObjectsDiff(obj1, obj2);
     const [innerKeyDiff] = diff.children;
-    expect(innerKeyDiff.type).toBe('equal');
+    expect(innerKeyDiff.type).toBe("equal");
   });
 
-  it('inner key type should be deleted', () => {
+  it("inner key type should be deleted", () => {
     const obj1 = {
       key: {
-        innerKey: 'foo',
+        innerKey: "foo",
       },
     };
 
@@ -55,21 +55,39 @@ describe('comparator', () => {
 
     const [diff] = getObjectsDiff(obj1, obj2);
     const [innerKeyDiff] = diff.children;
-    expect(innerKeyDiff.type).toBe('deleted');
+    expect(innerKeyDiff.type).toBe("deleted");
   });
-  it('inner key type should be added', () => {
+
+  it("inner key type should be added", () => {
     const obj1 = {
       key: {},
     };
 
     const obj2 = {
       key: {
-        innerKey: 'foo',
+        innerKey: "foo",
       },
     };
 
     const [diff] = getObjectsDiff(obj1, obj2);
     const [innerKeyDiff] = diff.children;
-    expect(innerKeyDiff.type).toBe('added');
+    expect(innerKeyDiff.type).toBe("added");
+  });
+
+  it("inner key type should be tree", () => {
+    const obj1 = {
+      key: {
+        innerKey: "bar",
+      },
+    };
+
+    const obj2 = {
+      key: {
+        innerKey: "foo",
+      },
+    };
+
+    const [keyDiff] = getObjectsDiff(obj1, obj2);
+    expect(keyDiff.type).toBe("tree");
   });
 });
