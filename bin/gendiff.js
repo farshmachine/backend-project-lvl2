6dfe57283496f254ae1543d836482c43684279cf
farshmachine <m4ghty@yandex.ru>
2022-06-17 23:43:51 +0300

    changed entry point

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -14,6 +14,7 @@ program
   .action((filepath1, filepath2, { format }) => {
     const diff = getDiff(format, filepath1, filepath2);
     console.log(diff);
+    console.log(typeof getDiff);
   });
 
 program.parse();
