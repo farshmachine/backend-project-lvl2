e78c129c180291ae248da74d82d4d46183a453ef
farshmachine <m4ghty@yandex.ru>
2022-07-21 16:27:44 +0300

    added debug

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ import { getFormater } from './formaters.js';
 import getObjectDiff from './comparator.js';
 
 export default (format, ...paths) => {
+  console.log(paths);
   const [obj1, obj2] = paths.map((path) => {
     const resolvedPath = resolvePath(path);
     const ext = extname(resolvedPath).slice(1);
-    const data = readFileSync(resolvedPath);
+    const raw = readFileSync(resolvedPath);
     const parser = parsers[ext];
-
-    return parser(data);
+    const data = parser(raw);
+    return data;
   });
+  console.log('obj1', obj1);
+  console.log('obj2', obj2);
 
   const diff = getObjectDiff(obj1, obj2);
   const formater = getFormater(format);
